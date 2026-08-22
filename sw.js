d473c42f9f11d7d3428955b2dd54c3ed57e0b0b3
sourmilkman <127108956+sourmilkman@users.noreply.github.com>
2026-08-22 20:26:36 +0100

    Update manually refined frame apertures

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='miniature-frame-selector-v2';
+const CACHE='miniature-frame-selector-v3';
 const SHELL=['./','./index.html','./styles.css','./fixes.css','./app.js','./manifest.webmanifest','./icons/icon.svg','./image-library/catalogue-image-map.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
